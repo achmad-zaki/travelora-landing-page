fa2848f3aecb8956e9b2242d1e97d48fb5084440
ahmad zaki <achmadzaki10d@gmail.com>
2025-01-01 21:22:23 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1rem',
+          sm: '2rem',
+          lg: '4rem',
+          xl: '5rem',
+          '2xl': '6rem',
+        },
+      },
+      colors: {
+        primary: {
+          50: '#71AAE4',
+          100: '#5F9FE1',
+          200: '#3D8BDA',
+          300: '#2677C9',
+          400: '#1F62A6',
+          500: '#194E84',
+          600: '#103255',
+          700: '#071626',
+        }
+      }
+    },
+    fontFamily: {
+      poppins: ['Poppins, serif']
+    }
+  },
+  plugins: [],
+}
