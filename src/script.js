9cf2ae9363d427dd0891eae538eb91499bfd3756
ahmad zaki <achmadzaki10d@gmail.com>
2025-01-09 19:52:22 +0700

    feat: filter

--- src/script.js
+++ src/script.js
@@ -9,12 +9,11 @@ const handleCloseSidebar = () => {
     sideBar.classList.remove('translate-x-0')
     sideBar.classList.add('translate-x-full')
 }
-
 // filter
-// const handleFilter = () => {
-//     let destinationInput = document.getElementById('destination')
-//     if (destinationInput) {
-
-//         destinationInput.value
-//     }
-// }
+const handleFilter = () => {
+    const destinationInput = document.getElementById('destination').value
+    const checkInInput = document.getElementById('check_in').value
+    const checkOut = document.getElementById('check_out').value
+    const roomGuestInput = document.getElementById('room_guest').value
+    console.log(destinationInput, checkInInput, checkOut, roomGuestInput)
+}
