b6e5c4e8b19ae3957aa946247e3187abc2592cc7
ahmad zaki <achmadzaki10d@gmail.com>
2025-01-12 20:16:55 +0700

    done: section testimonial

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -1,10 +1,10 @@
 let sideBar = document.querySelector('#sidebar')
 const handleOpenSidebar = () => {
-    sideBar.classList.add('translate-x-0')
+    sideBar.classList.add('translate-x-0', 'shadow-xl')
     sideBar.classList.remove('translate-x-full')
 }
 
 const handleCloseSidebar = () => {
-    sideBar.classList.remove('translate-x-0')
+    sideBar.classList.remove('translate-x-0', 'shadow-xl')
     sideBar.classList.add('translate-x-full')
 }
